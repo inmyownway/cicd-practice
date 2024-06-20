7edd5f24fee0c15e7d8e619a81f7a73a0ca17b11
inmyownway <wjdals05070@gmail.com>
2024-06-21 00:58:41 +0900

    init

---
+++ src/test/java/com/example/cicd_practice/CicdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
